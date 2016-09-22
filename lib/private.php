6c51a10ce9b0631d13d04dc2084ef1c59867aaf5
Thiago Ricieri <thiago.ricieri@gmail.com>
2016-09-21 21:47:48 -0300

    Cria script para normalizar WIN

---
+++ lib/private.php
@@ -0,0 +1,18 @@
+<?php
+
+$authRico = new CredenciaisRico();
+$authXP = new CredenciaisXP();
+
+$authRico->login = "thiagoricieri"
+$authRico->senha = "psr23055";
+$authRico->assinatura = "psr23055";
+//
+
+
+$authXP->login = "510871";
+$authXP->senha = "?levee-ana-nosy!";
+$authXP->assinatura = "81167913";
+//
+$authXP->servicoUrl = "http://homebroker.tororadar.com.br/main/";
+$authXP->urlLogin = "login/authDefaultBroker.go";
+$authXP->urlEnviarOrdem = "trade/sendOrder.go";
